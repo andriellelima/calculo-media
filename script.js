e3f13b0b45c85d853b832b07b4e8d29e02751d57
andrielle.lima <andrielle.lima10@gmail.com>
2021-02-22 20:17:12 -0500

    correçao link

--- script.js
+++ script.js
@@ -26,13 +26,6 @@ function calcular() {
       resultado.innerHTML += `<img src="img/avatar-passou.svg">`
       resultado.innerHTML += `<p id="media"> Média: ${media} <p>`
       resultado.innerHTML += `<p>PARABÉNS! Você passou e não fez mais que sua obrigação rsrs</p>`
-      resultado.innerHTML += `<a href="home.html" type="button" class="btn btn-secondary btn-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-              <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-            </svg>
-            Inicio
-          </a>`
       resultado.innerHTML += `<a href="media.html" type="button" class="btn btn-secondary mt-3 mb-3">Retornar</a>`
 
 
@@ -53,13 +46,6 @@ function calcular() {
       resultado.innerHTML += `<img src="img/avatar-final.svg">`
       resultado.innerHTML += `<p id="media"> Média: ${media} <p>`
       resultado.innerHTML += `<p>IH RAPAZ! Vai precisar fazer final, não se apavore ESTUDE você precisa de <strong> ${final} pontos </strong> </p>`
-      resultado.innerHTML += `<a href="home.html" type="button" class="btn btn-secondary btn-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-              <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-            </svg>
-            Inicio
-          </a>`
       resultado.innerHTML += `<a href="media.html" type="button" class="btn btn-secondary mt-3 mb-3">Retornar</a>`
 
     } else {
